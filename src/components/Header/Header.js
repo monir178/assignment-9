587ec234f9f704477c71057c8584f11b47f07f12
Your Name <you@example.com>
2022-10-26 04:34:47 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,26 @@ import React from 'react';
 const Header = () => {
     return (
 
-        <div>
-            <div className=' h-100' style={{
-                backgroundImage: "url(" + "https://miro.medium.com/max/1400/0*Bz7Q4ik7cUerhvOD.jpg" + ")", backgroundPosition: 'center',
-                backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
-            }} >
 
-                <h4 className='text-4xl font-bold text-white shadow-gray-300'>Let's know Your Knowledge</h4>
+        <div className=' h-100 my-5 py-5' style={{
+            backgroundImage: "url(" + "https://images.pexels.com/photos/2653362/pexels-photo-2653362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" + ")", backgroundPosition: 'center',
+            backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundBlendMode: 'hue'
+        }} >
 
-
+            <div className='ml-4'>
+                <h4 className='text-4xl font-bold text-white shadow-gray-300 pb-3'>Let's know Your Knowledge</h4>
+                <p className='text-gray-200 font-semibold text-md py-3'>
+                    How do you describe website development?
+                    Image result for learn about web development <br /> paragraph for website heading
+                    Web development is the building and maintenance of websites; <br /> it's the work that happens behind the scenes <br /> to make a website look great, work fast and perform well with a seamless user experience. <br /> Web developers, or 'devs', do this by using a variety of coding languages.
+                </p>
             </div>
 
+
+
         </div>
+
+
 
     );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <span className="font-extrabold text-4xl tracking-tight">Quizzy Buzzers</span>
             </div>
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-blue-700  hover:text-white hover:border-white">
+                <button className="flex items-center px-3 py-2 mt-2.5 border rounded text-blue-700  hover:text-white hover:border-white">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                 </button>
             </div>
