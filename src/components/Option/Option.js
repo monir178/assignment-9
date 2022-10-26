6b1f538bffdb6dcb408e4b673449d952dcd4cb38
Your Name <you@example.com>
2022-10-27 03:56:11 +0600

    responsive done

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -3,13 +3,21 @@ import React from "react";
 const Option = ({ option }) => {
   return (
     <div>
-      <div className="border-2 rounded-lg border-blue-200 w-full text-center bg-gray-200 py-8 px-4 ">
-        <input type="radio" />
-
-        {option}
+      <div className="border-2 rounded-lg border-blue-200 w-full text-center bg-gray-200 py-8 px-4 hover:bg-slate-300 flex gap-3 align-center">
+        <input className="" type="radio" />
+        <label className="font-semibold" htmlFor="">
+          {option}
+        </label>
       </div>
     </div>
   );
 };
 
 export default Option;
+
+// <button onClick={handleAnswer} className=' text-zinc-700 flex align-center  border rounded-lg border-green-700 m-3 lg:p-10 md:p-5 p-4  hover:bg-slate-100'>
+
+// <input className='h-6 w-4 ' type="radio" name=""     />
+// <label className='mx-5 text-xl'  >{option}</label>
+
+// </button>
