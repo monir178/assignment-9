9883b4b0db9dee314863f8dcd4497c620215a71a
Your Name <you@example.com>
2022-10-27 05:02:04 +0600

    statistics components added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h3>This is Statistics</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>This is Statistics component</h3>
+    </div>
+  );
 };
 
 export default Statistics;
