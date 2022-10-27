7d3e36e8a7a8cccac0a6f03865df3fe8bdc7dd51
Your Name <you@example.com>
2022-10-27 22:06:47 +0600

    statistics added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { PureComponent } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import {
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const Statistics = () => {
+  const quizesData = useLoaderData();
+  const {data} = quizesData;
+  const [{name, total, id}] = data;
+  console.log(name, id, total)
+
+
   return (
+  
     <div>
-      <h3>This is Statistics component</h3>
+    <h2 className='text-4xl text-gray-600 text-center font-semibold mb-12 mt-4'>This Stats is about the total number of quizes of the topics.</h2> 
+      <ResponsiveContainer width="80%" height="80%" aspect={2}>
+    <ComposedChart
+      width={400}
+      height={400}
+      data={data}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20,
+      }}
+    >
+      <CartesianGrid stroke="#f5f5f5" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="total" barSize={80} fill="#413ea0" />
+      <Line type="monotone" dataKey="total" stroke="#ff7300" />
+    </ComposedChart>
+  </ResponsiveContainer>
     </div>
+ 
   );
 };
 
