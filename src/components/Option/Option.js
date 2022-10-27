4801f725c6fdffb071cca775da3e9a278573049e
Your Name <you@example.com>
2022-10-27 06:27:05 +0600

    option component done

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -16,10 +16,3 @@ const Option = ({ option, correctAnswer }) => {
 };
 
 export default Option;
-
-// <button onClick={handleAnswer} className=' text-zinc-700 flex align-center  border rounded-lg border-green-700 m-3 lg:p-10 md:p-5 p-4  hover:bg-slate-100'>
-
-// <input className='h-6 w-4 ' type="radio" name=""     />
-// <label className='mx-5 text-xl'  >{option}</label>
-
-// </button>
