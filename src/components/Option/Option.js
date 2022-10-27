502b5a33cd3cc198891e97f505c3afdf47855f64
Your Name <you@example.com>
2022-10-27 20:15:48 +0600

    eye button toast added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,16 +1,34 @@
 import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Option = ({ option, correctAnswer }) => {
-    console.log(correctAnswer);
+ console.log(correctAnswer);
+
+ const showToastInRadio = () =>{
+  if(correctAnswer === option){
+    toast.success('Good Job! Correct Answer', {
+      position: toast.POSITION.TOP_CENTER
+    }
+    )
+  }
+  else{
+    toast.error('Hey Developer, Wrong Answer!', {
+   position: toast.POSITION.TOP_CENTER
+});
+   }
+ }
 
   return (
     <div>
-      <div className="border-2 rounded-lg border-blue-200 w-full text-center bg-gray-200 py-8 px-4 hover:bg-slate-300 flex gap-3 align-center">
-        <input className="" type="radio" />
+      <button className="border-2 rounded-lg border-blue-200 w-full text-center bg-gray-200 py-8 px-4 hover:bg-slate-300 flex gap-3 align-center">
+        <input  onClick={showToastInRadio} className="h-6 w-6" type="radio" />
         <label className="font-semibold" htmlFor="">
           {option}
         </label>
-      </div>
+        <ToastContainer/>
+      </button>
+     
     </div>
   );
 };
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Option from "../Option/Option";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = ({ question, id }) => {
   const {correctAnswer} = question;
     // console.log(question);
     // console.log(correctAnswer);
+    const showCorrectAnsInEye = () =>{
+      toast.info(`Answer : ${correctAnswer}`, {
+        position: toast.POSITION.TOP_LEFT
+    });
+    }
 
   return (
     <div className="mt-5 w-3/4 mx-auto shadow-lg py-8 px-4 rounded-lg">
-      <svg
+   <div  className="flex justify-end mr-2">
+   <svg onClick={showCorrectAnsInEye}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 22 22"
@@ -21,6 +29,7 @@ const Questions = ({ question, id }) => {
           d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
         />
       </svg>
+   </div>
 
       <p className="text-xl font-semibold text-center text-blue-700 py-8 ">
         Question: {question.question}
